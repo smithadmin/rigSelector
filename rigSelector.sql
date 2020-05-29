5c261309bcedeb62393d0da97bd24dc365bd8de3
Bill Smith <smithadmin+git@gmail.com>
2020-05-29 16:18:46 -0400

    Another update to the DB, this time to clean up how information is added.  Should be the last change to the structure for a while.  Next is to add data.

--- rigSelector.sql
+++ rigSelector.sql
@@ -6,9 +6,9 @@ CREATE TABLE 'phyform' (
    "phyKey" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    "shape"  NVARCHAR(6) NOT NULL
 );
-INSERT INTO 'phyform' VALUES (1,'Base');
-INSERT INTO 'phyform' VALUES (2,'HT');
-INSERT INTO 'phyform' VALUES (3,'Mobile');
+INSERT INTO 'phyform' (shape) VALUES ('Base');
+INSERT INTO 'phyform' (shape) VALUES ('HT');
+INSERT INTO 'phyform' (shape) VALUES ('Mobile');
 COMMIT;
 
 BEGIN TRANSACTION;
@@ -17,12 +17,12 @@ CREATE TABLE 'brand' (
    "brandKey" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    "brandName" NVARCHAR(10) NOT NULL
 );
-INSERT INTO 'brand' VALUES(1,'Anytone');
-INSERT INTO 'brand' VALUES(2,'Baofeng');
-INSERT INTO 'brand' VALUES(3,'BTECH');
-INSERT INTO 'brand' VALUES(4,'Icom');
-INSERT INTO 'brand' VALUES(5,'Kenwood');
-INSERT INTO 'brand' VALUES(6,'Yaesu');
+INSERT INTO 'brand' (brandName) VALUES ('Anytone');
+INSERT INTO 'brand' (brandName) VALUES ('Baofeng');
+INSERT INTO 'brand' (brandName) VALUES ('BTECH');
+INSERT INTO 'brand' (brandName) VALUES ('Icom');
+INSERT INTO 'brand' (brandName) VALUES ('Kenwood');
+INSERT INTO 'brand' (brandName) VALUES ('Yaesu');
 COMMIT;
 
 BEGIN TRANSACTION;
@@ -31,12 +31,12 @@ CREATE TABLE 'digMode' (
    "digKey" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    "mode"   NVARCHAR(10) NOT NULL
 );
-INSERT INTO 'digMode' VALUES (1,'None');
-INSERT INTO 'digMode' VALUES (2,'DMR');
-INSERT INTO 'digMode' VALUES (3,'DStar');
-INSERT INTO 'digMode' VALUES (4,'NXDN');
-INSERT INTO 'digMode' VALUES (5,'C4FM');
-INSERT INTO 'digMode' VALUES (6,'P25');
+INSERT INTO 'digMode' (mode) VALUES ('None');
+INSERT INTO 'digMode' (mode) VALUES ('DMR');
+INSERT INTO 'digMode' (mode) VALUES ('DStar');
+INSERT INTO 'digMode' (mode) VALUES ('NXDN');
+INSERT INTO 'digMode' (mode) VALUES ('C4FM');
+INSERT INTO 'digMode' (mode) VALUES ('P25');
 COMMIT;
 
 BEGIN TRANSACTION;
@@ -44,41 +44,42 @@ DROP TABLE IF EXISTS 'options';
 CREATE TABLE 'options' (
    "optKey" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    "rigndx" INTEGER NOT NULL,
-   "options" NVARCHAR(15) NOT NULL
+   "option" NVARCHAR(15) NOT NULL
 );
-/* rigndx corresponds to rigs.rigNDX  */
-/* a zero value is for menu           */
-INSERT INTO 'options' VALUES (1,0,'APRS');
-INSERT INTO 'options' VALUES (2,0,'Bluetooth');
-INSERT INTO 'options' VALUES (3,0,'GPS');
-INSERT INTO 'options' VALUES (4,0,'Radio Receiver (78-108 MHz)');
-INSERT INTO 'options' VALUES (5,0,'Full Duplex');
-INSERT INTO 'options' VALUES (6,0,'Snapshot');
-INSERT INTO 'options' VALUES (7,0,'Band Scope');
-INSERT INTO 'options' VALUES (8,0,'Voice Record');
-INSERT INTO 'options' VALUES (9,7,'APRS');
-INSERT INTO 'options' VALUES (10,7,'Bluetooth');
-INSERT INTO 'options' VALUES (11,7,'GPS');
-INSERT INTO 'options' VALUES (12,6,'APRS');
-INSERT INTO 'options' VALUES (13,6,'GPS');
-INSERT INTO 'options' VALUES (14,6,'Snapshot');
-INSERT INTO 'options' VALUES (15,5,'APRS');
-INSERT INTO 'options' VALUES (16,5,'Bluetooth');
-INSERT INTO 'options' VALUES (17,5,'GPS');
-INSERT INTO 'options' VALUES (18,5,'Radio Receiver (78-108 MHz)');
-INSERT INTO 'options' VALUES (19,5,'Full Duplex');
-INSERT INTO 'options' VALUES (20,5,'Snapshot');
-INSERT INTO 'options' VALUES (21,5,'Band Scope');
-INSERT INTO 'options' VALUES (22,5,'Voice Record');
-INSERT INTO 'options' VALUES (23,3,'APRS');
-INSERT INTO 'options' VALUES (24,3,'GPS');
-INSERT INTO 'options' VALUES (25,3,'Voice Record');
+/*  rigndx corresponds to rigs.rigNDX               */
+/*  This may change, need to think about this one.  */
+/*  a zero value is for menu                        */
+INSERT INTO 'options' (rigndx, option) VALUES (0,'APRS');
+INSERT INTO 'options' (rigndx, option) VALUES (0,'Bluetooth');
+INSERT INTO 'options' (rigndx, option) VALUES (0,'GPS');
+INSERT INTO 'options' (rigndx, option) VALUES (0,'Radio Receiver (78-108 MHz)');
+INSERT INTO 'options' (rigndx, option) VALUES (0,'Full Duplex');
+INSERT INTO 'options' (rigndx, option) VALUES (0,'Snapshot');
+INSERT INTO 'options' (rigndx, option) VALUES (0,'Band Scope');
+INSERT INTO 'options' (rigndx, option) VALUES (0,'Voice Record');
+INSERT INTO 'options' (rigndx, option) VALUES (7,'APRS');
+INSERT INTO 'options' (rigndx, option) VALUES (7,'Bluetooth');
+INSERT INTO 'options' (rigndx, option) VALUES (7,'GPS');
+INSERT INTO 'options' (rigndx, option) VALUES (6,'APRS');
+INSERT INTO 'options' (rigndx, option) VALUES (6,'GPS');
+INSERT INTO 'options' (rigndx, option) VALUES (6,'Snapshot');
+INSERT INTO 'options' (rigndx, option) VALUES (5,'APRS');
+INSERT INTO 'options' (rigndx, option) VALUES (5,'Bluetooth');
+INSERT INTO 'options' (rigndx, option) VALUES (5,'GPS');
+INSERT INTO 'options' (rigndx, option) VALUES (5,'Radio Receiver (78-108 MHz)');
+INSERT INTO 'options' (rigndx, option) VALUES (5,'Full Duplex');
+INSERT INTO 'options' (rigndx, option) VALUES (5,'Snapshot');
+INSERT INTO 'options' (rigndx, option) VALUES (5,'Band Scope');
+INSERT INTO 'options' (rigndx, option) VALUES (5,'Voice Record');
+INSERT INTO 'options' (rigndx, option) VALUES (3,'APRS');
+INSERT INTO 'options' (rigndx, option) VALUES (3,'GPS');
+INSERT INTO 'options' (rigndx, option) VALUES (3,'Voice Record');
 COMMIT;
 
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS 'rigs';
 CREATE TABLE 'rigs' (
-   "rigNDX" INTEGER PRIMARY KEY NOT NULL,
+   "rigNDX" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    "model"  NVARCHAR(10) NOT NULL,
    "brand"  INTEGER NOT NULL,
    "shape"  INTEGER NOT NULL,
@@ -86,12 +87,12 @@ CREATE TABLE 'rigs' (
    "msrp"   REAL NOT NULL,
    "vlink"  NVARCHAR(200)
 );
-INSERT INTO 'rigs' VALUES (1,'FT-450D',6,1,1,739.95,'https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=102&encProdID=870B3CA7CFCB61E6A599B0EFEA2217E4');
-INSERT INTO 'rigs' VALUES (2,'TM-281A',5,3,1,149.95,'https://www.kenwood.com/usa/com/amateur/tm-281a/');
-INSERT INTO 'rigs' VALUES (3,'AT-878UV',1,2,2,208.99,'https://www.bridgecomsystems.com/collections/amateur-handheld-radios/products/anytone-at-d878uv-dual-band-dmr-handheld-radio-w-gps-programming-cable');
-INSERT INTO 'rigs' VALUES (4,'IC-718',4,1,1,679.95,'https://www.icomamerica.com/en/products/amateur/hf/718/default.aspx');
-INSERT INTO 'rigs' VALUES (5,'FT-3DR',6,2,5,499.95,'https://www.yaesu.com/indexvs.cfm?cmd=DisplayProducts&ProdCatID=111&encProdID=84807B1262BFED6AC816544D94D310E3&DivisionID=65&isArchived=0');
-INSERT INTO 'rigs' VALUES (6,'FT-2DR',6,2,5,393.99,'https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=111&encProdID=4A66D869E574453F343581B53E9FAB40&DivisionID=65&isArchived=0');
-INSERT INTO 'rigs' VALUES (7,'AT-D578IIIPro',1,3,2,399.99,'https://www.bridgecomsystems.com/products/at-d578uv');
+INSERT INTO 'rigs' (model, brand, shape, mode, msrp, vlink) VALUES ('FT-450D','Yaesu',1,1,739.95,'https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=102&encProdID=870B3CA7CFCB61E6A599B0EFEA2217E4');
+INSERT INTO 'rigs' (model, brand, shape, mode, msrp, vlink) VALUES ('TM-281A','Kenwood',3,1,149.95,'https://www.kenwood.com/usa/com/amateur/tm-281a/');
+INSERT INTO 'rigs' (model, brand, shape, mode, msrp, vlink) VALUES ('AT-878UV','Anytone',2,2,208.99,'https://www.bridgecomsystems.com/collections/amateur-handheld-radios/products/anytone-at-d878uv-dual-band-dmr-handheld-radio-w-gps-programming-cable');
+INSERT INTO 'rigs' (model, brand, shape, mode, msrp, vlink) VALUES ('IC-718','Icom',1,1,679.95,'https://www.icomamerica.com/en/products/amateur/hf/718/default.aspx');
+INSERT INTO 'rigs' (model, brand, shape, mode, msrp, vlink) VALUES ('FT-3DR','Yaesu',2,5,499.95,'https://www.yaesu.com/indexvs.cfm?cmd=DisplayProducts&ProdCatID=111&encProdID=84807B1262BFED6AC816544D94D310E3&DivisionID=65&isArchived=0');
+INSERT INTO 'rigs' (model, brand, shape, mode, msrp, vlink) VALUES ('FT-2DR','Yaesu',2,5,393.99,'https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=111&encProdID=4A66D869E574453F343581B53E9FAB40&DivisionID=65&isArchived=0');
+INSERT INTO 'rigs' (model, brand, shape, mode, msrp, vlink) VALUES ('AT-D578IIIPro','Anytone',3,2,399.99,'https://www.bridgecomsystems.com/products/at-d578uv');
 COMMIT;
 
